efd2bfcf94b87314d1c92d3ffc2cee1b14b79e2d
Prithvi9518 <prithvi9518@gmail.com>
2022-11-19 16:25:19 +0000

    :hammer: Add isMoving flag to stop/resume enemy movement

--- 2022_3DGED_GDEngine/Core/Components/3D/Behaviours/EnemyPatrolBehaviour.cs
+++ 2022_3DGED_GDEngine/Core/Components/3D/Behaviours/EnemyPatrolBehaviour.cs
@@ -11,12 +11,18 @@ namespace GD.Engine
         private List<Vector3> waypoints;
         private float enemyMovementSpeed;
         private int currentWaypointIndex = 0;
+        private bool isMoving;
 
-        public EnemyPatrolBehaviour(List<Vector3> waypoints, float enemyMovementSpeed)
+        public EnemyPatrolBehaviour(List<Vector3> waypoints, float enemyMovementSpeed) :
+            this(waypoints, enemyMovementSpeed, true)
+        {
+        }
+
+        public EnemyPatrolBehaviour(List<Vector3> waypoints, float enemyMovementSpeed, bool isMoving)
         {
             this.waypoints = waypoints;
-
             this.enemyMovementSpeed = enemyMovementSpeed;
+            this.isMoving = isMoving;
         }
 
         private void MoveToNextWaypoint(GameTime gameTime)
@@ -35,6 +41,7 @@ namespace GD.Engine
 
                 // Reference for finding target rotation:
                 // https://subscription.packtpub.com/book/game-development/9781849692403/4/ch04lvl1sec107/time-for-action-enemy-update-and-draw
+
                 float targetRotation = Convert.ToSingle(Math.Atan2(transform.translation.Z - currentWaypoint.Z,
                     transform.translation.X - currentWaypoint.X));
 
@@ -47,7 +54,8 @@ namespace GD.Engine
 
         public override void Update(GameTime gameTime)
         {
-            MoveToNextWaypoint(gameTime);
+            if (isMoving)
+                MoveToNextWaypoint(gameTime);
             //base.Update(gameTime);
         }
     }
