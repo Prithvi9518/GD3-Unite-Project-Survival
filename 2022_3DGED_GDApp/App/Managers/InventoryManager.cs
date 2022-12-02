6f37517d5485313505d8d1c5ca8d920f2c0ab9a6
Prithvi9518 <prithvi9518@gmail.com>
2022-12-02 18:01:52 +0000

    :construction: WIP - implement InventoryItem class and Add, Remove methods in InventoryManager

--- 2022_3DGED_GDApp/App/Managers/InventoryManager.cs
+++ 2022_3DGED_GDApp/App/Managers/InventoryManager.cs
@@ -1,6 +1,7 @@
 ﻿using GD.Engine;
 using GD.Engine.Events;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,12 +21,34 @@ namespace GD.App
 
     public class InventoryItem
     {
+        public InventoryItemData itemData { get; private set; }
+        public int amount { get; private set; }
+
+        public InventoryItem(InventoryItemData itemData)
+        {
+            this.itemData = itemData;
+            Increment();
+        }
+
+        public void Increment()
+        {
+            amount++;
+        }
+
+        public void Decrement()
+        {
+            amount--;
+        }
+    }
+
+    public struct InventoryItemData
+    {
         public string uniqueID;
         public string name;
         public ItemType itemType;
         public string description;
-        public int value;
-        public string cueName;  //"boom"
+        public Texture2D icon;
+        public string cueName;
     }
 
     public class InventoryManager : PausableDrawableGameComponent
@@ -42,10 +65,40 @@ namespace GD.App
         {
         }
 
+        public void Add(InventoryItemData itemData)
+        {
+            if (inventory.Exists(x => (itemData.uniqueID == x.itemData.uniqueID)))
+            {
+                inventory.Find(x => (itemData.uniqueID == x.itemData.uniqueID)).Increment();
+            }
+            else
+            {
+                inventory.Add(new InventoryItem(itemData));
+            }
+        }
+
+        public void Remove(InventoryItemData itemData)
+        {
+            Predicate<InventoryItem> uniqueIDMatch = x => (x.itemData.uniqueID == itemData.uniqueID);
+
+            if (inventory.Exists(uniqueIDMatch))
+            {
+                inventory.Find(uniqueIDMatch).Decrement();
+
+                InventoryItem item = inventory.Find(uniqueIDMatch);
+                if (item.amount <= 0)
+                    inventory.Remove(item);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Item does not exist in inventory");
+            }
+        }
+
         public void PrintInventory()
         {
             foreach (InventoryItem item in inventory)
-                Console.WriteLine("Item name: " + item.name);
+                Console.WriteLine("Item name: " + item.itemData.name);
         }
 
         public override void Update(GameTime gameTime)
